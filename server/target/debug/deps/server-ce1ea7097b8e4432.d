ca77e6ca13cef006725bb93797ed5060f8916ebd
radley.wells <radleywells@gmail.com>
2025-04-26 16:45:08 +0100

    remove target

--- server/target/debug/deps/server-ce1ea7097b8e4432.d
+++
@@ -1,5 +0,0 @@
-C:\Users\radle\Work\fitness-tracker\server\target\debug\deps\libserver-ce1ea7097b8e4432.rmeta: src\lib.rs
-
-C:\Users\radle\Work\fitness-tracker\server\target\debug\deps\server-ce1ea7097b8e4432.d: src\lib.rs
-
-src\lib.rs:
--- server/target/debug/server.d
+++
@@ -1,1 +0,0 @@
-C:\Users\radle\Work\fitness-tracker\server\target\debug\server.exe: C:\Users\radle\Work\fitness-tracker\server\src\main.rs
